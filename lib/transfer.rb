dc0524314558d99996db3310ef34f0a6888062e9
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-26 22:05:19 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,10 @@
 class Transfer
+  attr_accessor :sender, :receiver, :status, :transfer_amount
   
   def initialize(sender,receiver,amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
+    @transfer_amount = amount
   end
 end
