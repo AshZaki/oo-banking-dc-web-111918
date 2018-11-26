59648219043c8496f4578fe7843cc84b3017810b
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-26 22:56:03 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     elsif @sender.balance && @receiver.balance > @amount
       self.status = "complete"
-      return status
+      
     end
   end
 end
