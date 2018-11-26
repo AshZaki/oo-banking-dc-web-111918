f6047a51b932a1adaa06ce91ecae452cacd53bc4
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-26 22:26:49 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   
   def execute_transaction
     if @sender.status && @receiver.status = "open"
-      self.status == "complete"
+      self.status = "complete"
     end
   end
 end
